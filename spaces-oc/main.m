1860e20992c49c186f996f4d22350eb90141479e
Henning Dahlheim <hactar@cyberkraft.ch>
2021-06-22 18:42:04 +0200

    Initial Commit

---
+++ spaces-oc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  spaces-oc
+//
+//  Created by Henning Dahlheim on 22.06.21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
